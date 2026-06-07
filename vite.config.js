23cc5454aba01c0fc8a141425387f2116dd99372
viraSkill <viraskill@gmail.com>
2026-06-07 07:42:43 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
-// vite.config.js
+// // vite.config.js
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   test: {
+//     globals: true,                       // استفاده از توابع describe, test بدون import
+//     environment: 'jsdom',                // شبیه‌سازی DOM مرورگر
+//     setupFiles: './src/setupTests.js',   // فایل تنظیمات اولیه
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,                       // استفاده از توابع describe, test بدون import
-    environment: 'jsdom',                // شبیه‌سازی DOM مرورگر
-    setupFiles: './src/setupTests.js',   // فایل تنظیمات اولیه
-  },
+  // تعریف base اگر در sub-path دیپلوی می‌شود (معمولاً نیازی نیست)
 })
